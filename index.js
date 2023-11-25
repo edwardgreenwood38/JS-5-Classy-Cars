ce4c651cb8b7e315f5713b66d959317259d50f69
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-25 16:20:26 -0500

    completed

--- index.js
+++ index.js
@@ -7,4 +7,41 @@ class Vehicle {
     }
   }
             
-  
+
+  //  Sedan
+  class Sedan extends Vehicle {
+    constructor(model, backupCamera) {
+        super(model)
+        this.backupCamera = backupCamera
+    }
+  }
+
+
+// SUV
+class Suv extends Vehicle {
+    constructor(model, offroadPackage) {
+        super(model)
+        this.offroadPackage = offroadPackage
+    }
+}
+
+
+// truck
+class Truck extends Vehicle {
+    constructor(model, towingCapacity) {
+        super(model)
+        this.towingCapacity = towingCapacity
+    }
+}
+
+
+// esedan
+class EVSedan extends Vehicle {
+    constructor(model) {
+        super(model)
+    }
+
+    recharge() {
+        return this.model + " recharges"
+    }
+}
